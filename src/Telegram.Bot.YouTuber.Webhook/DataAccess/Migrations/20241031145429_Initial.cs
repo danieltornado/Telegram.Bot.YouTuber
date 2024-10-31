b84a289efdb518ecc9aae73111366b97e10b346b
Daniel St <danielstadnich@gmail.com>
2024-10-31 17:55:31 +0300

    Recreate db

---
+++ src/Telegram.Bot.YouTuber.Webhook/DataAccess/Migrations/20241031145429_Initial.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Telegram.Bot.YouTuber.Webhook.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sessions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ChatId = table.Column<long>(type: "bigint", nullable: true),
+                    MessageId = table.Column<int>(type: "integer", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Url = table.Column<string>(type: "character varying(2048)", maxLength: 2048, nullable: true),
+                    Json = table.Column<string>(type: "jsonb", nullable: true),
+                    JsonVideo = table.Column<string>(type: "jsonb", nullable: true),
+                    JsonAudio = table.Column<string>(type: "jsonb", nullable: true),
+                    Error = table.Column<string>(type: "text", nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IsCompleted = table.Column<bool>(type: "boolean", nullable: false),
+                    VideoId = table.Column<Guid>(type: "uuid", nullable: true),
+                    AudioId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sessions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Downloading",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    SessionId = table.Column<Guid>(type: "uuid", nullable: false),
+                    VideoTitle = table.Column<string>(type: "character varying(1024)", maxLength: 1024, nullable: true),
+                    VideoUrl = table.Column<string>(type: "character varying(2048)", maxLength: 2048, nullable: true),
+                    VideoQuality = table.Column<string>(type: "character varying(32)", maxLength: 32, nullable: true),
+                    VideoFormat = table.Column<string>(type: "character varying(16)", maxLength: 16, nullable: true),
+                    VideoExtension = table.Column<string>(type: "character varying(8)", maxLength: 8, nullable: true),
+                    AudioTitle = table.Column<string>(type: "character varying(1024)", maxLength: 1024, nullable: true),
+                    AudioUrl = table.Column<string>(type: "character varying(2048)", maxLength: 2048, nullable: true),
+                    AudioQuality = table.Column<string>(type: "character varying(32)", maxLength: 32, nullable: true),
+                    AudioFormat = table.Column<string>(type: "character varying(16)", maxLength: 16, nullable: true),
+                    AudioExtension = table.Column<string>(type: "character varying(8)", maxLength: 8, nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Error = table.Column<string>(type: "text", nullable: true),
+                    IsCompleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Downloading", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Downloading_Sessions_SessionId",
+                        column: x => x.SessionId,
+                        principalTable: "Sessions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Media",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    SessionId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Num = table.Column<int>(type: "integer", nullable: false),
+                    Title = table.Column<string>(type: "character varying(1024)", maxLength: 1024, nullable: true),
+                    InternalUrl = table.Column<string>(type: "character varying(2048)", maxLength: 2048, nullable: true),
+                    Type = table.Column<int>(type: "integer", nullable: false),
+                    Quality = table.Column<string>(type: "character varying(32)", maxLength: 32, nullable: true),
+                    Format = table.Column<string>(type: "character varying(16)", maxLength: 16, nullable: true),
+                    Extension = table.Column<string>(type: "character varying(8)", maxLength: 8, nullable: true),
+                    IsSkipped = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Media", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Media_Sessions_SessionId",
+                        column: x => x.SessionId,
+                        principalTable: "Sessions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Downloading_SessionId",
+                table: "Downloading",
+                column: "SessionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Media_SessionId",
+                table: "Media",
+                column: "SessionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Downloading");
+
+            migrationBuilder.DropTable(
+                name: "Media");
+
+            migrationBuilder.DropTable(
+                name: "Sessions");
+        }
+    }
+}
